abaf4530fa29de385b57ccac095ffda1d44d405a
yamidevs <yamisaaf@live.fr>
2022-03-02 22:15:36 +0100

    feat: add logger file

--- src/discord/services/commands/early-command.ts
+++ src/discord/services/commands/early-command.ts
@@ -4,10 +4,13 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import ICommand from "../interfaces/ICommand";
 import { TYPES } from "../../../types";
 import { encrypt } from "../../../utils/crypt";
+import { Logger } from "../../../utils/logger";
 
 @injectable()
 export class EarlyCommand implements ICommand {
-    constructor(@inject(TYPES.WEBSITE) private website: String, @inject(TYPES.PRIVATE_KEY) private privateKey: string) {}
+    constructor(@inject(TYPES.WEBSITE) private website: String, @inject(TYPES.PRIVATE_KEY) private privateKey: string
+        , @inject(TYPES.LOGGER) private logger: Logger,
+    ) { }
 
     readonly commandName: string = "early";
     readonly data: SlashCommandBuilder = new SlashCommandBuilder().setName(this.commandName).setDescription("Early bird catches the worm").setDefaultPermission(false);
@@ -21,7 +24,8 @@ export class EarlyCommand implements ICommand {
         const row = new MessageActionRow().addComponents(
             new MessageButton().setLabel("Connect Wallet").setURL(`${this.website.toString()}/${encrypt(command.user.id.toString(), this.privateKey)}`).setStyle("LINK")
         );
-
+        
+        this.logger.log('INFO',`${command.user.id.toString()} use early command`)
         await command.reply({ ephemeral: true, components: [row] });
     }
 }
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,28 @@
 import { injectable } from "inversify";
+import DailyRotateFile from 'winston-daily-rotate-file';
+import *  as  winston from 'winston';
 
 @injectable()
 class Logger {
+    private logger: winston.Logger = null;
+    constructor() {
+        const transport: DailyRotateFile = new DailyRotateFile({
+            filename: 'application-%DATE%.log',
+            dirname: './logs/',
+            datePattern: 'YYYY-MM-DD-HH',
+            zippedArchive: true,
+            maxSize: '20m',
+            maxFiles: '14d'
+        });
+        this.logger = winston.createLogger({
+            transports: [
+                transport
+            ]
+        });
+    }
     log(type: string, message: string): void {
-        const time = new Date().toISOString();
-        console.log(`${type} : ${time} -- ${message}`);
+        this.logger.info(message)
     }
 }
 
-export {Logger}
+export { Logger }
